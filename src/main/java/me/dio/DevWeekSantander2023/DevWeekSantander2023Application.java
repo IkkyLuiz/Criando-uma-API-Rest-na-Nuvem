ca973faa07f094d43919c4810e30f882f4d99d25
IkkyLuiz <ikkylh26@gmail.com>
2023-09-29 16:35:13 -0300

    first commit

---
+++ src/main/java/me/dio/DevWeekSantander2023/DevWeekSantander2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.DevWeekSantander2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekSantander2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeekSantander2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/DevWeekSantander2023/DevWeekSantander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.DevWeekSantander2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekSantander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
